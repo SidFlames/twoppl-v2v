bd8e0e9c97c934b80a316d83f5e103f8686e078c
aihirelens <sidflames13@gmail.com>
2026-07-10 21:22:24 +0530

    feat(phase4-config): integrate dotenv loading for secure Cloudinary uploads using env variables

--- lumora/lib/main.dart
+++ lumora/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'screens/create_profile_screen.dart';
 import 'services/voice_background_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
